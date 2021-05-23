e12225f5c70c9e010168883e3b189dfa98528430
Hayes Choy <hayeselnut2127@gmail.com>
2021-05-23 22:27:09 +1000

    basic elo system

---
+++ src/components/graphs/elo-graph.js
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import { Line } from 'react-chartjs-2';
+
+import { prettyPrint, toMinsSecs } from '../../helpers/date-helpers';
+import NYTButtonGroup from '../imitations/nyt-button-group';
+
+const style = {
+  family: 'Franklin, sans-serif',
+  size: 14,
+};
+
+const EloGraph = (props) => {
+  const { eloToDisplay, users, selectedUsers } = props;
+
+  const data = {
+    labels: Object.keys(eloToDisplay).map((date) => prettyPrint(date)),
+    datasets: selectedUsers.map((username) => {
+      const userColour = users.filter((user) => user.name === username)[0].colour;
+
+      return {
+        label: username,
+        data: Object.values(eloToDisplay).map((day) => day[username]),
+        backgroundColor: userColour,
+        borderColor: `${userColour}A0`,
+        spanGaps: true,
+        cubicInterpolationMode: 'monotone',
+        lineTension: 0.40,
+      };
+    }),
+  };
+
+  let delayed;
+  const options = {
+    animation: {
+      onComplete: () => {
+        delayed = true;
+      },
+      delay: (context) => {
+        let delay = 0;
+        if (context.type === 'data' && context.mode === 'default' && !delayed) {
+          delay = context.dataIndex * 50 + context.datasetIndex * 50;
+        }
+        return delay;
+      },
+    },
+    interaction: {
+      intersect: false,
+      mode: 'index',
+    },
+    scales: {
+      y: {
+        display: true,
+        // suggestedMax: 120,
+        // max: subminute ? 60 : null,
+        title: {
+          display: true,
+          text: 'Elo rating',
+        },
+      },
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        // callbacks: {
+        //   label: (context) => {
+        //     const username = context.dataset.label;
+        //     const time = context.parsed.y;
+        //     return `${username} - ${toMinsSecs(time)}`;
+        //   },
+        // },
+        itemSort: (a, b) => b.dataset.data[b.dataIndex] - a.dataset.data[a.dataIndex],
+        titleFont: style,
+        bodyFont: style,
+        fotterFont: style,
+      },
+    },
+    maintainAspectRatio: false,
+  };
+
+  return (
+    <>
+      <div style={{ flexGrow: 1 }}>
+        <Line
+          data={data}
+          options={options}
+          style={{ height: '50vh' }}
+        />
+      </div>
+    </>
+  );
+};
+
+EloGraph.propTypes = {
+  eloToDisplay: PropTypes.object,
+  users: PropTypes.array,
+  selectedUsers: PropTypes.array,
+};
+
+export default EloGraph;
--- src/components/leaderboard-stats-graph.js
+++ src/components/leaderboard-stats-graph.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import TimeGraph from './graphs/time-graph';
-import PositionGraph from './graphs/position-graph';
+import EloGraph from './graphs/elo-graph';
 
 import NYTButtonGroup from './imitations/nyt-button-group';
 import NYTSegment from './imitations/nyt-segment';
@@ -14,6 +14,7 @@ const timeButtons = [
 ];
 
 const graphTypeButtons = [
+  { content: 'By Elo', value: 'elo' },
   { content: 'By Time', value: 'time' },
   { content: 'By Position', value: 'position' },
 ];
@@ -22,31 +23,70 @@ const getStatsInDateRange = (stats, dateRange) => {
   if (dateRange === 'week') {
     return Object.entries(stats)
       .slice(-7)
-      .reduce((statsToDisplay, [date, dayStats]) => ({
-        ...statsToDisplay,
-        [date]: dayStats,
+      .reduce((output, [key, data]) => ({
+        ...output,
+        [key]: data,
       }), {});
   }
 
   if (dateRange === 'month') {
     return Object.entries(stats)
       .slice(-30)
-      .reduce((statsToDisplay, [date, dayStats]) => ({
-        ...statsToDisplay,
-        [date]: dayStats,
+      .reduce((output, [key, data]) => ({
+        ...output,
+        [key]: data,
       }), {});
   }
 
   return stats;
 };
 
+const calculateElo = (times) => {
+  const sum = times.reduce((a, b) => a + b, 0);
+  return 100 / sum;
+};
+
+const getElo = (stats, users) => {
+  const period = 7; // Use last n games to calculate
+  const elo = {};
+  const userTimeCache = users.reduce((cache, user) => ({ ...cache, [user.name]: [] }), {});
+
+  console.log('time cahce', userTimeCache);
+
+  Object.entries(stats).forEach(([date, dayStats]) => {
+    Object.entries(dayStats).forEach(([username, seconds]) => {
+      if (userTimeCache[username]?.length >= period) {
+        userTimeCache[username]?.shift();
+      }
+
+      userTimeCache[username]?.push(seconds);
+    });
+
+    const dayElo = {};
+    users.forEach((user) => {
+      if (userTimeCache[user.name]?.length >= period) {
+        dayElo[user.name] = calculateElo(userTimeCache[user.name]);
+      }
+    });
+
+    if (Object.keys(dayElo).length > 0) {
+      elo[date] = dayElo;
+    }
+  });
+
+  return elo;
+};
+
 const LeaderboardStatsGraph = (props) => {
   const { stats, users, selectedUsers } = props;
 
   const [dateRange, setDateRange] = useState('week');
-  const [graphContent, setGraphContent] = useState('time');
+  const [graphContent, setGraphContent] = useState('elo');
 
   const statsToDisplay = getStatsInDateRange(stats, dateRange);
+
+  const elo = getElo(stats, users);
+  const eloToDisplay = getStatsInDateRange(elo, dateRange);
 
   return (
     <NYTSegment>
@@ -54,8 +94,9 @@ const LeaderboardStatsGraph = (props) => {
         <NYTButtonGroup state={graphContent} setState={setGraphContent} buttons={graphTypeButtons} />
       </div>
 
+      {graphContent === 'elo' && <EloGraph eloToDisplay={eloToDisplay} users={users} selectedUsers={selectedUsers}/>}
       {graphContent === 'time' && <TimeGraph stats={statsToDisplay} users={users} selectedUsers={selectedUsers}/>}
-      {graphContent === 'position' && <PositionGraph stats={statsToDisplay} users={users} selectedUsers={selectedUsers}/>}
+      {/* {graphContent === 'position' && <PositionGraph stats={statsToDisplay} users={users} selectedUsers={selectedUsers}/>} */}
 
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} >
         <NYTButtonGroup state={dateRange} setState={setDateRange} buttons={timeButtons} />
